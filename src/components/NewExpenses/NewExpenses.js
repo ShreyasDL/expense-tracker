1d98868d1bf98ea19dc2c90ca4cfd19a0aef4eae
Shreyas <dlshreyas30@gmail.com>
2022-08-28 13:01:55 +0530

    Charts are added

--- src/components/NewExpenses/NewExpenses.js
+++ src/components/NewExpenses/NewExpenses.js
@@ -1,18 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseForm from "./ExpenseForm";
-import './NewExpenses.css';
+import "./NewExpenses.css";
 
 const NewExpenses = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
-        ...enteredExpenseData,
-        id: Math.random().toString()
+      ...enteredExpenseData,
+      id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
+    setIsEditing(false);
+  };
+
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+  const stopEditingHandler = () => {
+    setIsEditing(false);
   }
-    return (
+  return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}/>
+      {!isEditing && (
+        <button onClick={startEditingHandler}>Add New Expense </button>
+      )}
+      {isEditing && <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} onCancel={stopEditingHandler}/>}
     </div>
   );
 };
